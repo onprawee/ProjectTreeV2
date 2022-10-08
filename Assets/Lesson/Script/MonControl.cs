9ac15a95338334cef8bd1f7aff38aa154dcd5fe2
Ongsaa <natcha0619@hotmail.com>
2022-10-08 22:33:02 +0700

    อนิเมชันหมุนได้

--- Assets/Lesson/Script/MonControl.cs
+++ Assets/Lesson/Script/MonControl.cs
@@ -13,19 +13,29 @@ public class MonControl : MonoBehaviour
     float moveSpeed = 3f;
     int waypointIndex = 0;
 
+    //Animation
+    private Animator anim;
+    private Vector3 localScale;
+    
+
     public GameObject[] A; //node ที่ยังไม่ลงสี = Waypoint ใน Scence
     public GameObject[] nodecoler; //node ที่ลงสี 
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isRunning",false);
         transform.position = waypoints[waypointIndex].transform.position;
+
+        localScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        MovePosition();
     }
     void Move()
     {
@@ -35,24 +45,34 @@ public class MonControl : MonoBehaviour
         if(transform.position == waypoints[waypointIndex].transform.position){
             A[waypointIndex].SetActive(false); 
             nodecoler[waypointIndex].SetActive(true);
+
+            anim.SetBool("isRunning",false);
         }
+
         // if(transform.position == waypoints[waypointIndex].transform.position){
         //     waypointIndex +=1;
         //     Debug.Log(waypointIndex);
         // }
-
-        // if(waypointIndex==2){
-
-        // }
-        
         
         // if(waypointIndex == waypoints.Length){
         //     waypointIndex = 0;
         // }
     }
+    void  MovePosition(){
+        if(transform.position.x >= waypoints[waypointIndex].transform.position.x){
+            localScale.x = Mathf.Abs(localScale.x) * -1 ; //*-1 ทำให้หันซ้าย
+            transform.localScale = localScale;
+        }else if(transform.position.x <= waypoints[waypointIndex].transform.position.x){
+            localScale.x = Mathf.Abs(localScale.x); //หมุนกลับ 
+            transform.localScale = localScale;
+        }else{
+            
+        }
+    }
     public void Next(){
         if(transform.position == waypoints[waypointIndex].transform.position){
             waypointIndex +=1;
+            anim.SetBool("isRunning",true);
             Debug.Log(waypointIndex);
         }
     }
