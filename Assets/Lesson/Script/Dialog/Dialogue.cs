be98c8e2f5264b00bbc780813b6d1be7c2606ca6
Onprawee <onprawee@kkumail.com>
2023-01-14 23:20:39 +0700

    Dialogue Display : hide&show obj | hide button

--- Assets/Lesson/Script/Dialog/Dialogue.cs
+++ Assets/Lesson/Script/Dialog/Dialogue.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [System.Serializable]
 public class Dialogue
 {
-    public string[] sentences;
+    public DialogueObject[] dialogueObjects;
 }
+
+[System.Serializable]
+public class DialogueObject
+{
+    public string sentence;
+    public GameObject nextObj;
+}
+
--- Assets/Lesson/Script/Dialog/DialogueManager.cs
+++ Assets/Lesson/Script/Dialog/DialogueManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class DialogueManager : MonoBehaviour
 {
     public Text dialogueText;
     // Queue : FIFO => First In Fist Out 
-    private Queue<string> sentences;
+    private Queue<DialogueObject> dialogueObjects;
 
     //Class Dialog
     public Dialogue dialogue;
@@ -15,49 +16,74 @@ public class DialogueManager : MonoBehaviour
     //Type Effect delay
     public float delay = 0.1f;
 
+    public GameObject previousDiplayedObj;
+
     void Start()
     {
-        sentences = new Queue<string>();
+        dialogueObjects = new Queue<DialogueObject>();
 
         //Show First text In Array 
         StartDialogue(dialogue);
     }
     private void StartDialogue(Dialogue dialogue)
     {
-        sentences.Clear();
+        dialogueObjects.Clear();
         //ข้อความอธิบายทั้งหมดถูกเก็บไว้ใน Array => public Dialogue dialogue;
         //ทำการ วน Loop เพื่อดึงข้อความอธิบายเข้ามาใส่ใน Queue ทีละตัว => private Queue<string> sentences;
-        foreach (string sentence in dialogue.sentences)
+        foreach (var obj in dialogue.dialogueObjects)
         {
-            sentences.Enqueue(sentence);
+            var dialogObj = new DialogueObject();
+            dialogObj.sentence = obj.sentence;
+            dialogObj.nextObj = obj.nextObj;
+
+            dialogueObjects.Enqueue(dialogObj);
         }
 
         DisplayNextSentence();
     }
     public void DisplayNextSentence()
     {
+        if (previousDiplayedObj)
+        {
+            previousDiplayedObj.SetActive(false);
+            previousDiplayedObj = null;
+        }
+
         //ถ้าไม่มี ข้อความเหลืออยู่ในคิว : จะปิด Dialog 
-        if (sentences.Count == 0)
+        if (dialogueObjects.Count == 0)
         {
             return;
         }
         //กรณีที่มีข้อความเหลืออยู่ จะทำการดึงข้อความนั้นออกมาจากคิว  แล้วเอาไปใส่ในตัวแปร sentence 
-        string sentence = sentences.Dequeue();
+        var obj = dialogueObjects.Dequeue();
 
-        StopAllCoroutines(); //หยุดฟังก์ชันทั้งหมดที่ทำงานอยู่ 
+        StopAllCoroutines(); //หยุดฟังก์ชันทั้งหมดที่ทำงานอยู่
+
         //เรียกใช้ฟังก์ชัน TypeSentence(sentence) ทำงาน 
-        StartCoroutine(TypeSentence(sentence));
+        StartCoroutine(TypeSentence(obj));
         Debug.Log("Dequeue : ");
     }
-    IEnumerator TypeSentence(string sentence)
+    IEnumerator TypeSentence(DialogueObject obj)
     {
         //แสดงข้อความทีละตัวอักษร โดยสามารถระบุ Delay ได้ 
         dialogueText.text = "";
+        var sentence = obj.sentence;
+        var nextObj = obj.nextObj;
+
+        var nextButton = GameObject.Find("NextText");
+        nextButton.SetActive(false);
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(delay);
         }
-    }
+        // ถ้า nextObj ไม่ใช่ null ให้แสดง nextObj ขึ้นมา
+        if (nextObj != null)
+        {
+            nextObj.SetActive(true);
+            previousDiplayedObj = nextObj;
+        }
 
+        nextButton.SetActive(true);
+    }
 }
