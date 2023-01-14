3ee73a5cab7b0037412e8451da1dcce7a8e31bef
Onprawee <onprawee@kkumail.com>
2023-01-15 01:38:37 +0700

    remove unused imports

--- Assets/Game/Script/PuzzleScipt/PiecesScript.cs
+++ Assets/Game/Script/PuzzleScipt/PiecesScript.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.UI;
 
 public class PiecesScript : MonoBehaviour
 {
--- Assets/Game/Script/Travesal/PlayerPressButton.cs
+++ Assets/Game/Script/Travesal/PlayerPressButton.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerPressButton : MonoBehaviour
