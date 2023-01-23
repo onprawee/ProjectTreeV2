a9826684d6fb50d737d1222f4e058b7a6859cef7
Onprawee <onprawee@kkumail.com>
2023-01-23 17:19:57 +0700

    Edit Script Button Game

--- Assets/Menu/MenuGame/ButtonMenuGame.cs
+++ Assets/Menu/MenuGame/ButtonMenuGame.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,19 +8,46 @@ public class ButtonMenuGame : MonoBehaviour
     {
         SceneManager.LoadScene("Menu_Home");
     }
-
-    public void ButtonMenuPreorder()
+    //Game Jisaw
+    //Menu Game Jisaw => Menu Subject Jisaw => Game Page
+    public void GameJisaw()
     {
-        SceneManager.LoadScene("PreorderLevel");
+        SceneManager.LoadScene("Menu_subject_jisaw");
     }
 
-    public void ButtonMenuPostorder()
+    public void Subject_Jisaw_Preorder()
     {
-        SceneManager.LoadScene("PostorderLevel");
+        SceneManager.LoadScene("Preoder_jisaw");
     }
 
-    public void ButtonMenuInorder()
+    public void Subject_Jisaw_Inorder()
     {
-        SceneManager.LoadScene("InorderLevel");
+        // SceneManager.LoadScene("PreorderLevel");
+    }
+
+    public void Subject_Jisaw_Postorder()
+    {
+        // SceneManager.LoadScene("PreorderLevel");
+    }
+
+    //Game Traversal 
+    //Menu Game Traversal => Menu Subject Traversal => Menu_Level => Game Page
+    public void GameTraversal()
+    {
+        SceneManager.LoadScene("Menu_subject_travelsal");
+    }
+    public void Subject_Traversal_Preorder()
+    {
+        SceneManager.LoadScene("PreorderLevel_Traversal");
+    }
+
+    public void Subject_Traversal_Inorder()
+    {
+        // SceneManager.LoadScene("InorderLevel_Traversal");
+    }
+
+    public void Subject_Traversal_Postorder()
+    {
+        // SceneManager.LoadScene("PostorderLevel_Traversal");
     }
 }
