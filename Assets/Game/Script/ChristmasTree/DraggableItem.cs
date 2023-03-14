d1b876eb54dca00310d82d880ab2c52fbdf7341d
Onprawee <onprawee@kkumail.com>
2023-03-14 15:51:59 +0700

    Drop hind , drag show

--- Assets/Game/Script/ChristmasTree/DraggableItem.cs
+++ Assets/Game/Script/ChristmasTree/DraggableItem.cs
@@ -5,24 +5,36 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 {
     public Image image;
     [HideInInspector] public Transform parentAfterDrag;
+
+    [HideInInspector] public InventorySlot inventorySlot;
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
+        //Debug.Log("OnBeginDrag");
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
         image.raycastTarget = false;
+
+
+        if (parentAfterDrag.CompareTag("DecorateBox"))
+        {
+            inventorySlot = parentAfterDrag.GetComponent<InventorySlot>();
+            inventorySlot.gameObject.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+        }
+
+
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        //Debug.Log("OnDrag");
         transform.position = eventData.position;
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
+        //Debug.Log("OnEndDrag");
         transform.SetParent(parentAfterDrag);
         image.raycastTarget = true;
     }
